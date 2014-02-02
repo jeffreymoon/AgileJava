449629cbcfed06fe94655bd378d3c427f128e706
Jeffrey Moon <novmoon73@gmail.com>
2014-02-02 17:24:53 +0900

    change report by string to report by stream

--- src/sis/report/RosterReporter.java
+++ src/sis/report/RosterReporter.java
@@ -1,17 +1,23 @@
 package sis.report;
 
-import sis.studentinfo.CourseSession;
-import sis.studentinfo.Student;
 import static sis.report.ReportConstant.NEWLINE;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+import sis.studentinfo.Session;
+import sis.studentinfo.Student;
 
 public class RosterReporter {
 
-//    public static final String NEWLINE = System.getProperty("line.separator");
-    public static final String ROSTER_REPORT_HEADER = "Student" + NEWLINE + "-" + NEWLINE;
-    public static final String ROSTER_REPORT_FOOTER = NEWLINE + "# students = ";
-    private CourseSession session;
+    public static final String ROSTER_REPORT_HEADER = "Student%n-%n";
+    public static final String ROSTER_REPORT_FOOTER = "%n# students = %d%n";
+    private Session session;
+	private Writer writer;
 
-    public RosterReporter(CourseSession session) {
+    public RosterReporter(Session session) {
         this.session = session;
     }
 
@@ -37,5 +43,37 @@ public class RosterReporter {
     private void writeHeader(StringBuilder buffer) {
         buffer.append(ROSTER_REPORT_HEADER);
     }
+    
+    public void writeReport(String filename) throws IOException {
+    	Writer bufferedWriter = new BufferedWriter(new FileWriter(filename));
+    	try {
+    		writeReport(bufferedWriter);
+    	}
+    	finally {
+    		bufferedWriter.close();
+    	}
+    }
+
+	public void writeReport(Writer writer) throws IOException {
+		this.writer = writer;
+		writeHeader();
+		writeBody();
+		writeFooter();
+	}
+
+	private void writeFooter() throws IOException {
+		writer.write(String.format(ROSTER_REPORT_FOOTER
+				, session.getAllStudents().size()));
+	}
+
+	private void writeBody() throws IOException {
+		for (Student student : session.getAllStudents()) {
+			writer.write(String.format(student.getName() + "%n"));
+		}
+	}
+
+	private void writeHeader() throws IOException {
+		writer.write(String.format(ROSTER_REPORT_HEADER));
+	}
 
 }
--- src/sis/report/RosterReporterTest.java
+++ src/sis/report/RosterReporterTest.java
@@ -1,26 +1,58 @@
 package sis.report;
 
-import static sis.report.ReportConstant.NEWLINE;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
 import junit.framework.TestCase;
 import sis.studentinfo.Course;
 import sis.studentinfo.CourseSession;
 import sis.studentinfo.DateUtil;
+import sis.studentinfo.Session;
 import sis.studentinfo.Student;
 
 public class RosterReporterTest extends TestCase {
-
-	public void testRosterReport() {
-		CourseSession session = CourseSession.create(new Course("ENGL", "101"),
+	private Session session;
+	
+	protected void setUp() {
+		session = CourseSession.create(new Course("ENGL", "101"),
 				DateUtil.createDate(2003, 1, 6));
+		
 		session.enroll(new Student("A"));
 		session.enroll(new Student("B"));
-
-		String rosterReport = new RosterReporter(session).getReport();
-		System.out.println(rosterReport);
-		assertEquals(
-				RosterReporter.ROSTER_REPORT_HEADER + "A" + NEWLINE + "B"
-						+ NEWLINE + RosterReporter.ROSTER_REPORT_FOOTER + "2"
-						+ NEWLINE, rosterReport);
 	}
 
+	public void testRosterReport() throws IOException {
+		Writer writer = new StringWriter();
+		new RosterReporter(session).writeReport(writer);
+		assertReportContents(writer.toString());
+	}
+	
+	public void assertReportContents(String rosterReport) {
+		
+		assertEquals(
+				String.format(RosterReporter.ROSTER_REPORT_HEADER
+							+ "A%n"
+							+ "B%n"
+							+ RosterReporter.ROSTER_REPORT_FOOTER, 2)
+				, rosterReport);
+	}
+
+	public void testFileReport() throws IOException {
+		final String filename = "testFiledReport.txt";
+		new RosterReporter(session).writeReport(filename);
+		
+		StringBuffer buffer = new StringBuffer();
+		String line;
+		
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		while ((line = reader.readLine()) != null) {
+			buffer.append(String.format(line + "%n"));
+		}
+		reader.close();
+		
+		assertReportContents(buffer.toString());
+	}
 }
